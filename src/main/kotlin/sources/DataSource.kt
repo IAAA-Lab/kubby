ceae728c389978e0746a908e0bd857dc50646526
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2018-12-18 16:02:33 +0100

    Complete

---
+++ src/main/kotlin/sources/DataSource.kt
@@ -0,0 +1,34 @@
+package es.iaaa.kubby.sources
+
+import org.apache.jena.rdf.model.Model
+import java.io.Closeable
+
+/**
+ * A Facade interface for the DataSources.
+ * This allows to provide several implementations.
+ */
+interface DataSource : Closeable {
+    /**
+     * Initializes the source.
+     */
+    fun init()
+
+    /**
+     * Returns a subgraph describing one resource based on a relative IRI
+     */
+    fun describe(relativeIRI: String): Model
+}
+
+class EmptyDataSource(): DataSource {
+    override fun close() {
+        TODO("not implemented")
+    }
+
+    override fun describe(relativeIRI: String): Model {
+        TODO("not implemented")
+    }
+
+    override fun init() {
+        TODO("not implemented")
+    }
+}
