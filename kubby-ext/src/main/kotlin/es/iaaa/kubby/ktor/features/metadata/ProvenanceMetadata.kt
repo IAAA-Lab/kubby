3093892f16a87f13838af0aef99f1bd33dd481b9
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2019-02-03 16:25:12 +0100

    Added PROV ontology definitions

--- kubby-ext/src/main/kotlin/es/iaaa/kubby/ktor/features/metadata/ProvenanceMetadata.kt
+++ kubby-ext/src/main/kotlin/es/iaaa/kubby/ktor/features/metadata/ProvenanceMetadata.kt
@@ -5,9 +5,10 @@ import es.iaaa.kubby.ktor.features.Metadata
 import es.iaaa.kubby.ktor.features.MetadataProcesor
 import es.iaaa.kubby.rdf.addNsIfUndefined
 import es.iaaa.kubby.rest.api.ContentContext
+import es.iaaa.kubby.vocabulary.PROV
 import org.apache.jena.rdf.model.Model
-import org.apache.jena.sparql.vocabulary.FOAF
 import org.apache.jena.vocabulary.RDF
+import org.apache.jena.vocabulary.RDFS
 
 /**
  * Provenance metadata description.
@@ -18,27 +19,26 @@ class ProvenanceMetadata : MetadataProcesor {
         val date = model.createTypedLiteral(context.time)
         val document = model.getResource(context.page)
         val agent = model.createResource()
-        val prov = "http://www.w3.org/ns/prov#"
         val activity = model.createResource()
 
         model.apply {
-            addNsIfUndefined("prov", prov)
+            addNsIfUndefined("prov", PROV.uri)
             addNsIfUndefined("rdf", RDF.uri)
-            addNsIfUndefined("foaf", FOAF.NS)
+            addNsIfUndefined("rdfs", RDFS.uri)
             agent.apply {
-                addProperty(RDF.type, createResource(prov + "SoftwareAgent"))
-                addProperty(RDF.type, createResource(prov + "Agent"))
-                addLiteral(FOAF.name, projectDescription.softwareName)
+                addProperty(RDF.type, PROV.SoftwareAgent)
+                addProperty(RDF.type, PROV.Agent)
+                addLiteral(RDFS.label, projectDescription.softwareName)
             }
             document.apply {
-                addProperty(RDF.type, createResource(prov + "Entity"))
-                addProperty(createProperty(prov, "wasGeneratedBy"), activity)
-                addLiteral(createProperty(prov, "generatedAtTime"), date)
+                addProperty(RDF.type, PROV.Entity)
+                addProperty(PROV.wasGeneratedBy, activity)
+                addLiteral(PROV.generatedAtTime, date)
             }
             activity.apply {
-                addProperty(RDF.type, createResource(prov + "Activity"))
-                addProperty(createProperty(prov, "generated"), document)
-                addProperty(createProperty(prov, "wasAssociatedWith"), agent)
+                addProperty(RDF.type, PROV.Activity)
+                addProperty(PROV.generated, document)
+                addProperty(PROV.wasAssociatedWith, agent)
             }
         }
     }
